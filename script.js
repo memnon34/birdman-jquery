a589f1ce31d84e538c516356c13f0c68964dcbcc
Daniel Llinas <danllinas@gmail.com>
2015-07-09 11:04:16 -0400

    Codecademy Project. Added dropdown menu.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var main = function() {
-  
-}
+  $('.dropdown img').click(function(){
+    $('.dropdown-menu').toggle();
+  });
+}         //This is probably the majority of the kind of things 
+//you'll be doing with JS when you make rails apps. 
+
+
  
 $(document).ready(main);
